5c5de95f0f66a1b324004d1909e61ffcf05f2d87
Nick Jackson <nijackson@lincoln.ac.uk>
2013-01-03 14:12:14 +0000

    Buildings CSV and proper MIME type declarations!

--- application/controllers/render.php
+++ application/controllers/render.php
@@ -36,4 +36,45 @@ class Render extends CI_Controller {
 		fclose($fp);
 		
 	}
+	
+	public function buildings_csv()
+	{
+	
+		echo 'Buildings CSV' . PHP_EOL;
+		
+		$buildings = json_decode(file_get_contents($_SERVER['NUCLEUS_BASE_URI'] . 'buildings?access_token=' . $_SERVER['NUCLEUS_TOKEN'] . '&limit=10000'));
+		
+		$fp = fopen('data/buildings.csv', 'w');
+		
+		fputcsv($fp, array(
+			'Estates Code',
+			'Name',
+			'Location',
+			'Location Code',
+			'Build Year',
+			'Latitude',
+			'Longitude',
+			'OSM Way ID'
+		));
+		
+		foreach ($buildings->results as $building)
+		{
+		
+			$building_data = json_decode(file_get_contents($_SERVER['NUCLEUS_BASE_URI'] . 'buildings/id/' . $building->id . '?access_token=' . $_SERVER['NUCLEUS_TOKEN']));
+		
+			fputcsv($fp, array(
+				$building_data->result->estates_code,
+				$building_data->result->name,
+				$building_data->result->location !== NULL ? $building_data->result->location->name : NULL,
+				$building_data->result->location !== NULL ? $building_data->result->location->estates_code : NULL,
+				$building_data->result->build_year !== NULL ? $building_data->result->build_year : NULL,
+				$building_data->result->latitude !== NULL ? $building_data->result->latitude : NULL,
+				$building_data->result->longitude !== NULL ? $building_data->result->longitude : NULL,
+				$building_data->result->osm_way_id !== NULL ? $building_data->result->osm_way_id : NULL
+			));
+		}
+		
+		fclose($fp);
+		
+	}
 }
